f9a36adb1ebfd8d5b29539f838104e241b83fce5
dchamroeun <chamroeurn.duch@gaea-sys.com>
2018-11-28 17:22:40 +0700

    add style css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './views/app.component.html',
+  styleUrls: ['./css/styles.css','./css/responsive.css','./css/bootstrap.css',
+  './css/ionicons.css','./css/fluidbox.min.css']
 })
 export class AppComponent {
   title = 'personal-cv';
